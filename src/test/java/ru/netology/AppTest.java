3637cdc1f05454ef6371e729dedce1f7aeda5886
Alex Baskakau <lenovos6000v4@gmail.com>
2023-05-23 12:27:58 +0100

    Dependenses added

--- src/test/java/ru/netology/AppTest.java
+++ src/test/java/ru/netology/AppTest.java
@@ -12,21 +12,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
-    private static WebDriver driver;
+    private  WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+       // ChromeOptions options = new ChromeOptions();
+       // options.addArguments("--disable-dev-shm-usage");
+       // options.addArguments("--no-sandbox");
+       // options.addArguments("--headless");
+       // driver = new ChromeDriver(options);
     }
 
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -36,7 +41,7 @@ public class AppTest {
     }
 
     @Test
-    void shouldTestSomething() throws InterruptedException {
+    void shouldTestSomething() {
         // throw new UnsupportedOperationException();
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Джек Дэниэлс");
@@ -45,7 +50,6 @@ public class AppTest {
         driver.findElement(By.cssSelector("[type=button]")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        Thread.sleep(50000);
         assertEquals(expected, actual);
 
     }
